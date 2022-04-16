dea52e4dc54922cfdac4b868193947bff971833e
Lucas Santos <39504808+lsantoss@users.noreply.github.com>
2022-04-16 16:07:38 -0300

    Iniciando CustomerServiceTest

---
+++ ServerASMX/ServerASMX.Test.Unit/Customers/Services/CustomerServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using ServerASMX.Test.Base.Base;
+
+namespace ServerASMX.Test.Unit.Customers.Services
+{
+    internal class CustomerServiceTest : DatabaseTest
+    {
+        private readonly CustomerService _customerService;
+
+        public CustomerServiceTest() => _customerService = new CustomerService();
+
+        [Test]
+        public void Add_Success()
+        {
+            
+        }
+    }
+}
