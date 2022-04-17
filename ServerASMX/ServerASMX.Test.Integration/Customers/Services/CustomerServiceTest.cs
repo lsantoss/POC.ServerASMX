056d2c3a7eee5aecfebc15b88de9836abc92eef9
Lucas Santos <39504808+lsantoss@users.noreply.github.com>
2022-04-17 20:16:04 -0300

    Iniciando ServerASMX.Test.Integration

---
+++ ServerASMX/ServerASMX.Test.Integration/Customers/Services/CustomerServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using ServerASMX.Test.Integration.CustomerService;
+using System;
+
+namespace ServerASMX.Test.Integration.Customers.Services
+{
+    internal class CustomerServiceTest
+    {
+        [Test]
+        public async void Add_Success()
+        {
+            var client = new CustomerServiceSoapClient();
+
+            var command = new CustomerAddCommand()
+            {
+                Name = "Lucas",
+                Birth = new DateTime(1995, 07, 14),
+                Gender = EGender.Male,
+                CashBalance = 2151.32m
+            };
+
+            var response = await client.AddAsync(command);
+            var result = response?.Body?.AddResult;
+        }
+    }
+}
--- ServerASMX/ServerASMX.Test.Integration/Properties/AssemblyInfo.cs
+++ ServerASMX/ServerASMX.Test.Integration/Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("ServerASMX.Test.Unit")]
+[assembly: AssemblyTitle("ServerASMX.Test.Integration")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ServerASMX.Test.Unit")]
+[assembly: AssemblyProduct("ServerASMX.Test.Integration")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: Guid("f73ecd25-d0f0-4789-af1c-1a5793319406")]
+[assembly: Guid("5ce22451-358e-423f-9e03-538a48a8f9f0")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
