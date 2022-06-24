afea447a9f0ab2c40df6f0515ba491d918684cf9
Lucas Santos <39504808+lsantoss@users.noreply.github.com>
2022-06-24 13:27:54 -0300

    Craindo separação POC.ServerASMX.Domain.Test.Unit e POC.ServerASMX.Domain.Test.Integration

--- test/POC.ServerASMX.Domain.Test.Integration/Properties/AssemblyInfo.cs
+++ test/POC.ServerASMX.Domain.Test.Integration/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("POC.ServerASMX.Domain.Test.Unit")]
+[assembly: AssemblyTitle("POC.ServerASMX.Domain.Test.Integration")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("POC.ServerASMX.Domain.Test.Unit")]
+[assembly: AssemblyProduct("POC.ServerASMX.Domain.Test.Integration")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: Guid("d029fcb1-8d8a-4bdc-af67-5f79d255b9b2")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
